a6caef7a2847870e5a4aea225792e496f964296d
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-12-26 16:32:19 -0800

    added full user info into modal in UsersShow.jsx

--- src/UsersShow.jsx
+++ src/UsersShow.jsx
@@ -3,9 +3,15 @@ export function UsersShow(props) {
     <div>
       <h2>User Information</h2>
       <p>Name: {props.user.user_name}</p>
-      <p>Url: {props.user.user_email}</p>
-      {/* <p>Width: {props.user.width}</p>
-      <p>Height: {props.user.height}</p>  */}
+      <p>Email: {props.user.user_email}</p>
+      <p>Phone Number: {props.user.user_phn_num}</p>
+      <p>Bio: {props.user.user_bio}</p>
+      <p>LinkedIn URL: {props.user.user_linkedin_url}</p>
+      <p>Twitter Handle: {props.user.user_twitter_handle}</p>
+      <p>Website URL: {props.user.user_website_url}</p>
+      <p>Resume URL: {props.user.user_resume_url}</p>
+      <p>Github URL: {props.user.user_github_url}</p>
+      <p>Photo_URL: {props.user.user_photo_url}</p>
     </div>
   );
 }
